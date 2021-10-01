498c108c0c2f45dec1db10693d79ed9d6fd12d82
Kumar Shashank <34642119+1711shashank@users.noreply.github.com>
2021-10-01 23:22:24 +0530

    A_ABC_String

---
+++ A_ABC_String.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                          long long
+#define pb                          push_back
+#define ff                          first
+#define ss                          second
+
+#define mem(arr,val)                memset(arr, val, sizeof(arr))
+#define ps(n,l)                     fixed<<setprecision(l)<<n
+#define lcm(a,b)                    (a/__gcd(a,b))*b
+
+#define fl(i,a,b)                   for(ll i=a; i<b; i++)
+#define fr(i,a,b)                   for(ll i=b-1; i>=a; i--)
+
+#define ins(s)                      string s;          cin>>s;
+#define in1(a)                      ll a;              cin>>a;
+#define in2(a, b)                   ll a, b;           cin >> a >> b;
+#define in3(a, b, c)                ll a, b, c;        cin >> a >> b >> c;
+#define in4(a, b, c, d)             ll a, b, c, d;     cin >> a >> b >> c >> d;
+
+#define InVec(v,n)                  vector<ll> v(n); fl(i,0,n) cin>>v[i];
+#define InArr(a,n)                  ll a[n]; fl(i,0,n) cin>>a[i];
+#define InArrSum(a,n,sum)           ll a[n]; fl(i,0,n) {cin>>a[i]; sum += a[i];}
+#define sz(s)                       s.size()
+
+#define dsc                         greater<ll>()
+#define all(x)                      (x).begin(), (x).end()
+
+#define println(n)                  cout << n << "\n"   
+#define printls(n)                  cout << n << " "
+#define print(n)                    cout << n
+#define ln                          cout << endl
+
+
+#define printArrVec(a)              for(auto it:a) cout<<it<<" "
+#define p2(a,b);                    cout<<a<<" "<<b<<endl;                                      
+#define p3(a,b,c);                  cout<<a<<" "<<b<<" "<<c<<endl; 
+#define YES                         println("YES")
+#define Yes                         println("Yes")
+#define NO                          println("NO")
+#define No                          println("No")
+
+#define mod 1000000007
+
+void solve(){
+    ins(s);
+    ll n = sz(n);
+    ll a=0,b=0,c=0;
+    fl(i,0,sz(s)){
+        if(s[i]=='A') a++;
+        else if(s[i]=='B') b++;
+        else c++;
+    } 
+    if(a!=n/2 || b!=n/2 || c!=n/2) {
+        NO;
+        return;
+    }
+    
+    char ch;
+    if(a>=b && a>=c) ch = 'A';
+    else if(b>=a && b>=c) ch = 'B';
+    else ch = 'C';
+
+    stack<char> st1;
+    stack<char> st2;
+    fl(i,0,sz(s)){
+        if(s[i] == ch ) 
+    }
+    
+}
+
+//int32_t 
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+
+    //in1(T);  while(T--) 
+        solve();
+        
+    return 0;
+    // return 0LL;
+}
